ab91c5342228ea4310868c3b3bee196a9f7f9ac9
Zioq <jr.roberthan@gmail.com>
2021-03-18 14:00:48 -0700

    update to fill data dynamically

--- index.js
+++ index.js
@@ -11,16 +11,52 @@ const url = require("url");
 const data = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+// Import html pages
+const homepage = fs.readFileSync(`${__dirname}/pages/index.html`, "utf-8");
+const card = fs.readFileSync(`${__dirname}/pages/card.html`, "utf-8");
+const product = fs.readFileSync(`${__dirname}/pages/product.html`, "utf-8");
+
+// METHODS
+const replaceTemplate = (temp, product) => {
+
+
+  let output = temp.replace(/{PRODUCT_NAME}/g,product.productName);
+  output = output.replace(/{PRODUCT_DESCRIPTION}/g,product.description);
+  output = output.replace(/{PRODUCT_IMG}/g,product.image);
+  output = output.replace(/{PRODUCT_FROM}/g,product.from);
+  output = output.replace(/{PRODUCT_NUTRIENTS}/g,product.nutrients);
+  output = output.replace(/{PRODUCT_QUANTITY}/g,product.quantity);
+  output = output.replace(/{PRODUCT_PRICE}/g,product.price);
+  output = output.replace(/{PRODUCT_DESCRIPTION}/g,product.description);
+  output = output.replace(/{PRODUCT_ID}/g,product.id);
+
+  if(!product.organic) output = output.replace(/{NOT_ORGANIC}/g, 'not-organic');
+
+  return output;
+}
 //Create server
 const server = http.createServer((req, res) => {
   // Set the URL path
   const path = req.url;
 
+  /* HOMEPAGE */
   if (path === "/" || path === "/home") {
-    res.end("This is home");
-  } else if (path === "/product") {
+
+    // Specify that we are sending back HTML to the browser.
+    res.writeHead(200,{'Content-type':'text/html'});
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(card,el)).join('');
+    const ouput = homepage.replace('{PRODUCT_CARDS}',cardsHtml);
+
+    res.end(ouput);
+    
+  } 
+  /* PRODUCT PAGE */
+  else if (path === "/product") {
     res.end("This is product");
-  } else if (path === "/api") {
+  } 
+  /* API */
+  else if (path === "/api") {
     if (data) {
       // Specify that we are sending back JSON to the browser.
       res.writeHead(200, {
